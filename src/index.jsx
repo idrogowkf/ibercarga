7d8029a159c5463db5ac70a0eb6f964df5c1268a
idrogowkf <idrogowkf@gmail.com>
2025-09-07 17:26:31 +0200

    build: fix Tailwind pipeline (postcss/tailwind configs + css import)

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
+﻿// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css";     // ← IMPORTANTE
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
