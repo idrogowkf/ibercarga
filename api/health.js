6cbcfb80486db120f93cc2393636d493b18dbe2d
idrogowkf <idrogowkf@gmail.com>
2025-09-10 22:47:12 +0200

    feat(api): ensure api/* present in main (health/ping/send-quote) + vercel.json

--- api/health.js
+++ api/health.js
@@ -3,3 +3,4 @@ module.exports = (req, res) => {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.end(JSON.stringify({ ok: true, time: new Date().toISOString() }));
 };
+// touch Wed Sep 10 22:47:11     2025
